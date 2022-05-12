ec3ae2aad8f83fa989a501eac476430759344965
Brett Yoncak <byoncak@gmail.com>
2022-05-12 15:22:28 -0500

    curse you, github pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  pages: {
+    index: {
+      entry: "src/main.js",
+      template: "src/index.html",
+      filename: "index.html",
+    },
+  },
   transpileDependencies: true,
   publicPath: "/",
 });
